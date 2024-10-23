5fd8e4c1b700d8f28e8adc41b1ae828969e4f81f
PabloFC <pablofc_68@hotmail.com>
2024-10-23 18:26:56 +0200

    adding css and app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,49 @@
+import { useState } from "react";
 import "./App.css";
+import axios from "axios";
 
 function App() {
+  const [input, setInput] = useState("");
+  const [weather, setWheather] = useState({
+    loading: false,
+    data: {},
+    error: false,
+  });
+
+  const search = (e) => {
+    if (e.key === "Enter") {
+      setInput("");
+      setWheather({ ...weather, loading: true });
+      axios
+        .get("", {
+          params: {},
+        })
+        .then((res) => {
+          console.log(res);
+        })
+        .catch((err) => {
+          console.log(err)
+          setWheather({ ...weather, data: {}, err: true });
+        });
+    }
+  };
+
   return (
     <div className="App">
-      <div className="weather-app"></div>
+      <div className="weather-app">
+        <div className="city-search">
+          <input
+            type="text"
+            name=""
+            className="city"
+            placeholder="Enter city name..."
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+            onKeyDown={search}
+            // https://openweathermap.org/
+          />
+        </div>
+      </div>
     </div>
   );
 }
