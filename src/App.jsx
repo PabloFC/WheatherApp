5f566008513b978bb79191ecb4d4ce443b6e611d
PabloFC <pablofc_68@hotmail.com>
2024-10-24 12:36:21 +0200

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,56 @@
+// https://openweathermap.org/
 import { useState } from "react";
 import "./App.css";
 import axios from "axios";
+import { Oval } from "react-loader-spinner";
 
 function App() {
   const [input, setInput] = useState("");
-  const [weather, setWheather] = useState({
+  const [weather, setWeather] = useState({
     loading: false,
     data: {},
     error: false,
   });
 
-  const search = (e) => {
+  const toDate = () => {
+    const months = [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December",
+    ];
+    const currentDate = new Date();
+    const date = `${currentDate.getDate()} ${months[currentDate.getMonth()]}`;
+    return date;
+  };
+
+  const searchInput = (e) => {
     if (e.key === "Enter") {
       setInput("");
-      setWheather({ ...weather, loading: true });
+      setWeather({ ...weather, loading: true });
       axios
-        .get("", {
-          params: {},
+        .get("https://api.openweathermap.org/data/2.5/weather", {
+          params: {
+            q: input,
+            units: "metric",
+            appId: "e1940f4dba6e90f03a07cb981a3c3fd9",
+          },
         })
         .then((res) => {
           console.log(res);
+          setWeather({ data: res.data, loading: false, error: false });
         })
         .catch((err) => {
-          console.log(err)
-          setWheather({ ...weather, data: {}, err: true });
+          console.log(err);
+          setWeather({ ...weather, data: {}, error: true });
         });
     }
   };
@@ -31,18 +58,51 @@ function App() {
   return (
     <div className="App">
       <div className="weather-app">
-        <div className="city-search">
+        <div className="city-searchInput">
           <input
             type="text"
-            name=""
             className="city"
-            placeholder="Enter city name..."
+            placeholder="Search city name..."
             value={input}
             onChange={(e) => setInput(e.target.value)}
-            onKeyDown={search}
-            // https://openweathermap.org/
+            onKeyDown={searchInput}
           />
         </div>
+        {weather.loading && (
+          <Oval type="Oval" color="yellow" height={70} width={70}></Oval>
+        )}
+        {weather.error && (
+          <div className="error-message">
+            <span>City not found</span>
+          </div>
+        )}
+        {weather && weather.data && weather.data.main && (
+          <div>
+            <div className="icon-temp">
+              <img
+                // width={100}
+                // height={100}
+                src={`https://openweathermap.org/img/wn/${weather.data.weather[0].icon}@2x.png`}
+                alt="weather icon"
+              />
+            </div>
+            <div className="degrees">
+              <h1>{Math.round(weather.data.main.temp)}°c</h1>
+            </div>
+            <div className="city-name">
+              <h2>
+                {weather.data.name}, <span>{weather.data.sys.country}</span>
+              </h2>
+            </div>
+            <div className="date">
+              <span>{toDate()}</span>
+            </div>
+            <div className="des-wind">
+              <p>{weather.data.weather[0].description.toUpperCase()}</p>
+              <p>Wind Speed: {weather.data.wind.speed}</p>
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
